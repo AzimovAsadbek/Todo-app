5476a03b3754dd49dd02b780ec01003cb9a87f43
shoxjaxon <shoxjaxon>
2023-11-08 14:42:29 +0500

    Todo Lists

---
+++ src/root/index.jsx
@@ -0,0 +1,252 @@
+import React, { useReducer, useState } from "react";
+import {
+  DateAdd,
+  DateAdd__btn,
+  DateAdd__input,
+  Date__item,
+  Main,
+  MainDate,
+  NotFound,
+  Search,
+  Search_btn,
+  Search_input,
+  Task_count,
+  Title,
+} from "./style";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMagnifyingGlass,
+  faPlus,
+  faFloppyDisk,
+} from "@fortawesome/free-solid-svg-icons";
+import img from "./img/search.gif";
+
+const mock = [
+  { id: 1, name: "Html", status: false, isEdited: false },
+  { id: 2, name: "JavaScript", status: false, isEdited: false },
+  { id: 3, name: "React Js", status: false, isEdited: false },
+  { id: 4, name: "React", status: false, isEdited: false },
+];
+
+const TodoApp = () => {
+  const reduser = (state, action) => {
+    switch (action.type) {
+      // Check
+      case "check": {
+        return state.map((value) => {
+          return value.id == action.payload
+            ? { ...value, status: !value.status }
+            : value;
+        });
+      }
+
+      // Delete
+      case "delite": {
+        return state.filter((v) => v.id !== action.payload);
+      }
+      case "search": {
+        return mock.filter((v) =>
+          v.name.toLowerCase().includes(action.payload.toLowerCase())
+        );
+      }
+
+      // Add
+      case "add": {
+        return [
+          ...state,
+          {
+            id: state[state.length - 1].id + 1,
+            name: action.payload,
+            status: false,
+            isEdited: false,
+          },
+        ];
+      }
+
+      // Edit
+      case "edit": {
+        return state.map((value) => {
+          if (value.id == action.payload) {
+            action.setSave(value.name);
+            return { ...value, isEdited: !value.isEdited };
+          } else return value;
+        });
+      }
+
+      // Save
+      case "save": {
+        return state.map((value) => {
+          if (value.id == action.payload) {
+            return { ...value, name: action.save, isEdited: false };
+          } else return value;
+        });
+      }
+
+      default:
+        return state;
+    }
+  };
+  const [base, dispatch] = useReducer(reduser, mock);
+  const [search, setSearch] = useState("");
+  const [add, setAdd] = useState("");
+  const [save, setSave] = useState("");
+  return (
+    <Main>
+      <Title>
+        <Task_count>{base.length} Tasks</Task_count>
+      </Title>
+
+      {/*  */}
+      <Search>
+        <Search_input
+          type="text"
+          placeholder="Search task"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <Search_btn
+          onClick={() => dispatch({ type: "search", payload: search })}
+        >
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </Search_btn>
+      </Search>
+
+      {/*  */}
+      <MainDate>
+        {base.length ? (
+          base.map((value) => {
+            return (
+              <Date__item key={value.id}>
+                <input
+                  id={value.id}
+                  type="checkbox"
+                  style={{ cursor: "pointer" }}
+                  onChange={() =>
+                    dispatch({ type: "check", payload: value.id })
+                  }
+                  key={value.id}
+                />
+                {value.isEdited ? (
+                  <input
+                    type="text"
+                    defaultValue={value.name}
+                    style={{
+                      height: "30px",
+                      width: "50%",
+                      fontSize: "20px",
+                      borderRadius: "6px",
+                      border: "1px solid black",
+                      paddingLeft: "6px",
+                    }}
+                    onChange={(e) => {
+                      setSave(e?.target?.value);
+                    }}
+                  />
+                ) : (
+                  <label
+                    htmlFor={value.id}
+                    style={
+                      !value.status
+                        ? { cursor: "pointer", fontSize: "20px" }
+                        : {
+                            cursor: "pointer",
+                            fontSize: "20px",
+                            color: "#838383",
+                            textDecoration: "line-through",
+                          }
+                    }
+                  >
+                    {value.status ? <i>{value.name}</i> : value.name}
+                  </label>
+                )}
+                <div>
+                  {!value.isEdited ? (
+                    <>
+                      {/* Edit */}
+                      <DateAdd__btn
+                        onClick={() => {
+                          dispatch({
+                            type: "edit",
+                            payload: value.id,
+                            setSave,
+                          });
+                        }}
+                      >
+                        e
+                      </DateAdd__btn>
+
+                      {/* Delete */}
+                      <DateAdd__btn
+                        onClick={() => {
+                          dispatch({
+                            type: "delite",
+                            payload: value.id,
+                          });
+                        }}
+                        delete="true"
+                      >
+                        d
+                      </DateAdd__btn>
+                    </>
+                  ) : (
+                    <>
+                      {/* SAVE */}
+                      <DateAdd__btn
+                        onClick={() =>
+                          dispatch({ type: "save", payload: value.id, save })
+                        }
+                      >
+                        <FontAwesomeIcon icon={faFloppyDisk} />
+                      </DateAdd__btn>
+                      {/* CANCEL */}
+                      <DateAdd__btn
+                        onClick={() => {
+                          dispatch({
+                            type: "edit",
+                            payload: value.id,
+                            setSave,
+                          });
+                        }}
+                        delete="true"
+                      >
+                        c
+                      </DateAdd__btn>
+                    </>
+                  )}
+                </div>
+              </Date__item>
+            );
+          })
+        ) : (
+          <NotFound>
+            <img src={img} alt="Eror"/>
+            <h2 style={{ color: "#122348", fontWeight: "900" }}>
+              Malumot mavjud emas!
+            </h2>
+          </NotFound>
+        )}
+      </MainDate>
+
+      {/* ADD */}
+      <DateAdd>
+        <DateAdd__input
+          type="text"
+          placeholder="Add Too..."
+          onChange={(e) => setAdd(e.target.value)}
+          value={add}
+        />
+        <DateAdd__btn
+          onClick={() => {
+            add.length
+              ? dispatch({ type: "add", payload: add })
+              : alert("Malumot kiriting!");
+            setAdd("");
+          }}
+        >
+          <FontAwesomeIcon icon={faPlus} />
+        </DateAdd__btn>
+      </DateAdd>
+    </Main>
+  );
+};
+
+export default TodoApp;
---
+++ src/root/style.js
@@ -0,0 +1,237 @@
+import styled from "styled-components";
+
+const Main = styled.div`
+  padding: 50px 20px;
+  border-radius: 5%;
+  min-width: 32vw;
+  max-width: 32vw;
+  box-shadow: rgba(0, 0, 0, 0.95) 0px 25px 30px 0px;
+  height: 90vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  background-color: white;
+  @media (max-width: 1320px) {
+    min-width: 70vh;
+    transition: 0.3s;
+  }
+  @media (max-width: 800px) {
+    max-width: 50vw;
+  }
+
+  @media (max-width: 600px) {
+    max-width: 50vw;
+    min-width: 60vw;
+  }
+  @media (max-width: 500px) {
+    max-width: 80vw;
+    min-width: 80vw;
+  }
+
+  @media (max-width: 300px) {
+    min-width: 90vw;
+    min-height: 80vh;
+  }
+`;
+
+const Title = styled.div`
+  width: 100%;
+  height: 15%;
+  border-bottom: 1px solid grey;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  color: green;
+  @media (max-width: 800px) {
+    width: 50%;
+  }
+`;
+
+const Task_count = styled.h1`
+  font-size: 40px;
+  margin: 15px;
+  white-space: nowrap;
+  @media (max-width: 800px) {
+    font-size: 40px;
+  }
+  @media (max-width: 600px) {
+    font-size: 35px;
+  }
+  @media (max-width: 500px) {
+    font-size: 30px;
+    flex-wrap: nowrap;
+  }
+  @media (max-width: 300px) {
+    font-size: 35px;
+  }
+  @media (max-height: 250px) {
+    font-size: 30px;
+    margin-bottom: 10px;
+  }
+`;
+
+const Search = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 5px;
+  justify-content: space-around;
+`;
+
+const Search_input = styled.input`
+  padding: 5px 20px;
+  width: 80%;
+  font-size: 20px;
+  outline: none;
+  color: green;
+  border: 2px solid green;
+  border-radius: 15px;
+  &::placeholder {
+    color: green;
+  }
+`;
+
+const Search_btn = styled.button`
+  background-color: transparent;
+  outline: none;
+  font-size: 20px;
+  color: green;
+  border: 2px solid transparent;
+  border-radius: 20px;
+  padding: 5px 20px;
+  &:hover {
+    background-color: green;
+    cursor: pointer;
+    color: white;
+  }
+  &:active {
+    background-color: rgba(0, 128, 0, 0.808);
+    cursor: pointer;
+    color: white;
+  }
+`;
+
+const MainDate = styled.div`
+  width: 100%;
+  height: 60%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  overflow: auto;
+`;
+
+const DateAdd = styled.div`
+  width: 95%;
+  display: flex;
+  gap: 5px;
+  color: #122348;
+  align-items: center;
+  height: 10%;
+  justify-content: space-between;
+`;
+
+const DateAdd__input = styled.input`
+  width: 80%;
+  padding: 10px;
+  font-size: 20px;
+  outline: none;
+  color: green;
+  border: 2px solid green;
+  border-radius: 15px;
+  &::placeholder {
+    color: green;
+    font-style: italic;
+  }
+  @media (max-width: 300px) {
+    font-size: 15px;
+  }
+  @media (max-height: 300px) {
+    font-size: 15px;
+    padding: 5px;
+  }
+  @media (max-height: 250px) {
+    font-size: 15px;
+    padding: 5px;
+  }
+`;
+
+const DateAdd__btn = styled.button`
+  background-color: transparent;
+  outline: none;
+  font-size: 20px;
+  color: ${(props) => (props.delete ? "red" : "green")};
+  border: 2px solid white;
+  border-radius: 20px;
+  padding: 10px 25px;
+  &:hover {
+    background-color: ${(props) => (props.delete ? "red" : "green")};
+    cursor: pointer;
+    color: white;
+    &:active {
+      background-color: ${(props) =>
+        props.delete ? "red" : "rgba(0, 128, 0, 0.808)"};
+      cursor: pointer;
+      color: white;
+    }
+  }
+  @media(max-width: 450px){
+    padding: 5px 13px;
+    border-radius: 12px;
+  }
+  @media (max-width: 300px) {
+    padding: 10px 15px;
+    font-size: 15px;
+  }
+
+  @media (max-height: 300px) {
+    padding: 10px 15px;
+    font-size: 15px;
+  }
+`;
+
+const NotFound = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  width: 70%;
+  text-align: center;
+`;
+
+const Date__item = styled.div`
+  padding: 2px;
+  width: 95%;
+  display: flex;
+  min-height: 25%;
+  align-items: center;
+  justify-content: space-between;
+  white-space: nowrap;
+  @media (max-width: 300px) {
+    font-size: 15px;
+  }
+  
+`;
+const img = styled.img`
+  @media (max-width: 1320px) {
+    width: 80%;
+  }
+  @media (max-width: 800px) {
+    width: 90%;
+  }
+`;
+
+export {
+  Main,
+  Title,
+  Task_count,
+  Search,
+  Search_input,
+  Search_btn,
+  MainDate,
+  DateAdd,
+  DateAdd__input,
+  DateAdd__btn,
+  NotFound,
+  Date__item,
+};
